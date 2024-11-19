43b37dfe702a9c4d4dff431a9a99831bdc76f50a
Aron Cachago <115675860+JhonattanAron@users.noreply.github.com>
2024-11-19 10:54:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from typing import Optional
+# server.py
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from starlette.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
+# Carga el modelo y el tokenizador
+model = AutoModelForSequenceClassification.from_pretrained("aronDFarkl/csaron-es")
+tokenizer = AutoTokenizer.from_pretrained("aronDFarkl/csaron-es")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class InputText(BaseModel):
+    text: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+# Configura CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite solicitudes de todos los orígenes (para desarrollo)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos los headers
+)
+
+@app.post("/predict")
+def predict(input: InputText):
+    # Tokeniza el texto
+    inputs = tokenizer(input.text, return_tensors="pt", padding=True, truncation=True)
+    with torch.no_grad():
+        logits = model(**inputs).logits
+        predicted_class = torch.argmax(logits, dim=-1).item()
+    return {"label": predicted_class}
